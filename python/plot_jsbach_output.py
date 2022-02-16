2513d0e61fe0ad18a28109d63a8d5737c6400c04
ziu1986 <stefanie.falk@lmu.de>
2022-02-16 10:48:20 +0100

    Add option to plot diff

--- python/plot_jsbach_output.py
+++ python/plot_jsbach_output.py
@@ -1,32 +1,47 @@
+from doctest import ELLIPSIS_MARKER
 import os, sys, glob
 import numpy as np
 import xarray as xr
 import yaml
 import matplotlib.pyplot as plt
+from matplotlib.colors import DivergingNorm
 import datetime as dt
 
-
-def plot_global(ax, data):
+def plot_global(ax, data, **kwargs):
     x = data['clon'].data
     y = data['clat'].data
     lai = data.data
 
-    img2 = ax.tricontourf(x,y,lai, 20, cmap=plt.cm.gist_earth_r) # choose 20 contour levels, just to show how good its interpolation is
+    b_diff = kwargs.pop('diff', False)
+    if b_diff:
+        map = plt.cm.BrBG
+        norm = DivergingNorm(0) #TwoSlopeNorm(0)
+        level = 40
+    else:
+        map = plt.cm.gist_earth_r
+        norm = None
+        level = 20
+
+    img2 = ax.tricontourf(x,y,lai, level, norm=norm, cmap=map) # choose 20 contour levels, just to show how good its interpolation is
     cbar = plt.colorbar(img2, ax=ax)
     cbar.set_label("LAI")
    
     ax.set_yticks(np.arange(-np.pi/2, np.pi/2+0.1, np.pi/4))
     ax.set_yticklabels(np.arange(-90,91,180/4.))
-    ax.set_xlabel("Longitude (deg)")
-    ax.set_ylabel("Latitude (deg)")
     ax.set_xticks(np.arange(-np.pi, np.pi+0.1,np.pi/3))
     ax.set_xticklabels(np.arange(-180, 181, 180/3.))
+    ax.set_xlabel("Longitude (deg)")
+    ax.set_ylabel("Latitude (deg)")
 
     ax.set_title(str(data.time.data)[:10])
 
-def plot_season(ax, data):
+def plot_season(ax, data, **kwargs):
+    b_diff = kwargs.pop('diff', False)
     data.sum(dim='ncells').plot()
-    ax.set_ylabel("LAI")
+    if b_diff:
+        ax.set_ylabel("$\Delta$LAI")
+    else:
+        ax.set_ylabel("LAI")
     ax.set_xlabel("Time")
 
 def read_data(src):
@@ -53,21 +68,28 @@ def main():
     # Load config
     config = init("config_plot_jsb_output.yml")
 
-    src = os.environ['MODELS'] + '/icon/' + config["src"]
-    print(src)
-    b_print = config["print"]
-  
-    data = read_data(src)
+    src_base = os.environ['MODELS'] + '/icon/'
     
+    b_print = config["print"]
+    b_diff = config["diff"]
+  
+    data = read_data(src_base + config["src"])
+    
+    if b_diff:
+        data = read_data(src_base + config["src2"]) - data
+
     # Plot it
     f, ax = plt.subplots(3,1, sharex=True, sharey=True)
+    
+    f.canvas.set_window_title("lai_global_map")
 
     for idata, iax in zip((data.sel(time='2000-02-01'),data.sel(time='2000-06-15'), data.sel(time='2000-12-01')), ax):
-        plot_global(iax, idata)
+        plot_global(iax, idata, diff=b_diff)
         
     f2, ax2 = plt.subplots(1)
+    f2.canvas.set_window_title("lai_global_time")
 
-    plot_season(ax2, data)
+    plot_season(ax2, data, diff=b_diff)
 
     plt.show(block=False)
 
