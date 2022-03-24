df48d1f124e14e671abc7deac548de4e5c46ac47
ziu1986 <stefanie.falk@lmu.de>
2022-03-24 09:13:11 +0100

    Increase verbosity

--- python/manipulate_land_input/generate_bc_landfrac.py
+++ python/manipulate_land_input/generate_bc_landfrac.py
@@ -3,12 +3,16 @@ import numpy as np
 import xarray as xr
 import yaml
 
-def extend_landfrac(data, var_name, var_name_new, fract):
+def extend_frac(data, var_name, var_name_new, fract):
     xr.set_options(keep_attrs=True)
     var_orig = data[var_name]
     var_new = var_orig.copy()*fract
     var_new.name = var_name_new
-    var_new.assign_attrs(long_name= var_new.long_name.replace(var_name[:-2], var_name_new[:-2]))
+    try:
+        var_new.assign_attrs(long_name= var_new.long_name.replace(var_name[:-2], var_name_new[:-2]))
+    except AttributeError:
+        print("Extending transitions...")
+
     return var_new
 
 def read_data(src):
@@ -25,6 +29,10 @@ def init(config_file):
     file.close()
     return(config_list)
 
+def save_data(data, target):
+    print("Saving... %s" % (target))
+    data.to_netcdf(target)
+
 def main():
     # Configuration
     config = init('config.yml')
@@ -38,13 +46,15 @@ def main():
     names.insert(0,old_name)
 
     data = read_data(src)
+    
+    print("Converting...")
 
     for ifract, iname in zip(fracts, names):
-        print(data[0][old_name])
-        #pft_tmp = extend_landfrac(data[0], old_name, iname, ifract)
-        #data[0][iname] = pft_tmp
+        print("%s x %1.3f -> %s" % (old_name, ifract, iname))
+        pft_tmp = extend_frac(data[0], old_name, iname, ifract)
+        data[0][iname] = pft_tmp
 
-    #data[0].to_netcdf(target)
+    save_data(data[0], target)
 
 if __name__ == "__main__":
     main()
